3f1ca54772b337a38e8ccf31c48c383773c8ea17
pawelek <k.pawelek@vp.pl>
2024-06-18 18:35:35 +0200

    update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { listContacts } = require("../../models/contacts");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("../../models/contacts");
 
 const router = express.Router();
 
@@ -13,16 +18,46 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  // const { contactId } = req.params;
   res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const getById = getContactById(contactId);
+    if (getById) {
+      return res.status(200).json(getById);
+    } else {
+      return res.status(404).json({ message: "Not found" });
+    }
+  } catch (e) {
+    next(e);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const contact = { ...req.body };
+    const createContact = addContact(contact);
+    if () {
+      return res.status(201).json({id, name, email, phone})
+    } else {
+      return res.status(400).json({"message": "missing required name - field"})
+    }
+  } catch (e) {
+    next(e);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const getById = removeContact(contactId);
+    if (getById) {
+      return res.status(200).json({ message: "contact deleted" });
+    } else {
+      return res.status(404).json({ message: "Not found" });
+    }
+  } catch (e) {
+    next(e);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
