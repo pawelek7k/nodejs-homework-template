f111eb9386ccbbdcadb8a91bb85307cef4c03150
pawelek <k.pawelek@vp.pl>
2024-07-02 13:37:39 +0200

    Create usersSchema.js

---
+++ usersSchema.js
@@ -0,0 +1,27 @@
+const mangoose = require("mangoose");
+const Schema = mangoose.Schema;
+
+const usersSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+});
